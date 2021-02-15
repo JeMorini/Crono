99a60c8169ab73cca02b70f12bafd166120d06eb
JeMorini <55816927+JeMorini@users.noreply.github.com>
2021-02-15 20:58:37 -0300

    Original

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+
+constructor(props){
+  super(props);
+  this.state = {
+    numero: 0,
+    botao: 'VAI',
+    ultimo: null,
+  };
+
+  this.timer = null
+  this.vai = this.vai.bind(this);
+  this.limpar = this.limpar.bind(this);
+
+}
+
+vai(){
+
+  if(this.timer != null){
+    clearInterval(this.timer);
+    this.timer = null;
+    this.setState({botao: 'VAI'})
+  }else{
+    this.setState({botao: 'PARAR'})
+    this.timer = setInterval( () =>{
+      this.setState({numero: this.state.numero + 0.1})
+    }, 100)
+    }
+  }
+
+limpar(){
+  this.setState({numero: 0, ultimo: this.state.numero.toFixed(2)})
+}
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Image 
+        source={require('./src/cronometro.png')}
+        style={styles.cronometro}
+        />
+        <Text style={styles.timer}>{this.state.numero.toFixed(2)}</Text>
+        <View style={styles.btnArea}>
+          <TouchableOpacity style={styles.btn} onPress={this.vai}>
+            <Text style={styles.btnTexto}>{this.state.botao}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn} onPress={this.limpar}>
+            <Text style={styles.btnTexto}>LIMPAR</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.areaUltima}>
+          <Text style={styles.textCorrida}>Ultimo Tempo: {this.state.ultimo}</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  container:{
+    flex:1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#00aeef'
   },
+  timer:{
+    marginTop: -160,
+    color: '#ffffff',
+    fontSize: 65,
+    fontWeight: 'bold'
+  },
+  btnArea:{
+    flexDirection: 'row',
+    marginTop: 70,
+    height: 40
+  },
+  btn:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 9
+  },
+  btnTexto:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00aeef'
+  },
+  areaUltima:{
+    marginTop: 40,
+  },
+  textCorrida:{
+    fontSize: 25,
+    fontStyle: 'italic',
+    color: '#FFF'
+  }
 });
+
+export default App;
